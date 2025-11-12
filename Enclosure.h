88aea26a9434841e968a4d55e009829876bc47a5
norhannabeih <norhanmohamed0203@gmail.com>
2025-11-12 15:19:12 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,60 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
 
+#include <iostream>
+
 #include "Animal.h"
+#include "instructor.h"
 
-//define enclosure class here 
+class Enclosure {
+private:
+    /**
+     *
+     */
+    int capacity;
+    Animal*a;
+    int currentcount;
+    int currentanimal;
+    int maxanimal;
+
+public:
+    // Default constructor
+    Enclosure() {
+    }
+
+    Enclosure(const char * lion, int i, const char * str);
+
+    // Destructor
+    ~Enclosure() {
+        delete[] a;
+    }
 
 
+    void addAnimal(Animal*a) {
+        if (currentanimal < maxanimal) {
+            Animal[currentanimal] = a;
+            currentanimal++;
+        } else {
+            cout << "Course is full!" << endl;
+        }
+    }
 
 
-
-
-
+    void displayAnimal(){
+        cout << "Capacity: " << capacity << endl;
+        cout << "Animal: " *a << endl;
+        cout << "Currentcount: ";
+        if (currentcount == 0)
+            cout << "None" << endl;
+        else {
+            for (int i = 0; i < currentcount; i++) {
+                cout << a.Animal[i].getName();
+                if (i != currentcount - 1)
+                    cout << ", ";
+            }
+            cout << endl;
+        }
+    }
+};
 
 #endif
