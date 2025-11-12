72370ea38e34d8a25f0823f07c1ac5db0f2a4fbc
norhannabeih <norhanmohamed0203@gmail.com>
2025-11-12 15:16:24 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,88 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+#ifndef Animal_H
+#define Animal_H
 
+#include <iostream>
 #include <string>
+#include "Animal.h"
 using namespace std;
 
-//define animal class here 
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
+
+public:
+    // Default constructor
+    Aniaml() {
+        name = "";
+        age = 0;
+        isHungry = false;
+    }
 
 
+    Animal(string n, int i) {
+        name = n;
+        age = i;
+        isHungry = true;
+    }
+
+    Animal();
+
+
+    ~Animal() {
+
+    }
+
+
+    void setName(string n) {
+        name = n;
+    }
+    void setId(int i) {
+        age = i;
+    }
+
+    string getName() {
+        return name;
+    }
+    int getId() {
+        return age;
+    }
+    class Mammal : public Animal {
+        public:
+        Mammal() : Animal() {
+            string furColor;
+        }
+    };
+
+    class Bird : public Animal {
+    public:
+        Bird() : Animal() {
+            float wingSpan;
+        }
+    };
+    class Reptile : public Animal {
+    public:
+        Reptile() : Animal() {
+            bool isVenomous;
+        }
+    };
+
+
+
+    virtual void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "IsHungry: " << isHungry << endl;
+    }
+    void feed() {
+        if (isHungry==true) {
+            cout << "Hungry" << endl;
+        }
+        else {
+            cout << "Unhungry" << endl;
+        }
+    }
+};
 
 #endif
