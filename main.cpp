2c96a2b5d44751fc55bc5652be5ce06d631f0415
norhannabeih <norhanmohamed0203@gmail.com>
2025-11-12 15:22:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,33 @@
 using namespace std;
 
 
-// ============== MAIN FUNCTION ==============
+
 int main() {
-    
-    
+     Enclosure Enc1("Lion", 5, "Hungry");
+    Enclosure Enc2("Parrot", 2, "Not Hungry");
+    Enclosure Enc3("Snake", 3, "Hungry");
+
+
+
+    Visitor v1("Sarah Ali", '3');
+
+
+    a1.addAnimal(a1);
+    a2.addAnimal(a2);
+
+    a1.displayAnimalInfo();
+    a2.displayAnimalInfo();
+
+
+    // Display Instructor Info
+    cout << "\nInstructor Info:" << endl;
+    inst.display();
+
+    // Display Visitor Info
+    cout << "\nVisitor Info:" << endl;
+    v1.display();
+    cout << endl;
+
     return 0;
 }
+    
