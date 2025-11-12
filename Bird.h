e0cd6a11d2f6b465edf308ca85c859649216f159
norhannabeih <norhanmohamed0203@gmail.com>
2025-11-12 15:17:21 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,6 @@
-#ifndef BIRD_H
-#define BIRD_H
-
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
-#endif
+class Bird : public Animal {
+    public:
+        Bird() : Animal() {
+            float wingSpan;
+        }
+    };
