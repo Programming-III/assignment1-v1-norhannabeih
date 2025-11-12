520697622243c3ceff3409a44b17d19c76d0dabc
norhannabeih <norhanmohamed0203@gmail.com>
2025-11-12 15:21:08 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,14 +1,22 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
 #include <string>
-using namespace std;
 
-//define visitor class here
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor();
+    virtual ~Visitor();
+    virtual void accept(Visitor* v);
 
+    void displayInfo() {
+        cout << "visitorname: " << visitorName << endl;
+        cout << "ticketsbought: " << ticketsBought << endl;
 
-
-
+    }
+};
 
 
 
